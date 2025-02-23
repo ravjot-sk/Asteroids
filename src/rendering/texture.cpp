94df3ca2c384a3bf6eea16183c79fe0aa9b05c23
ravjot-sk <ravjot.kohli@uni-muenster.de>
2025-02-23 22:37:40 +0100

    imgTexture and textTexture classes of parent basicTexture

--- src/rendering/texture.cpp
+++ src/rendering/texture.cpp
@@ -1,7 +1,7 @@
 #include "texture.hpp"
 #include<iostream>
 
-gameTexture::gameTexture(){
+/* gameTexture::gameTexture(){
 
     //Initialise variables
     aTexture = NULL;
@@ -69,4 +69,114 @@ void gameTexture::render(SDL_Renderer* gameRenderer, int x, int y, double angle,
 
     //Add to renderer
     SDL_RenderCopyEx(gameRenderer,aTexture,NULL,&renderRect,angle,center,SDL_FLIP_NONE);
+}
+ */
+//constructor to initialise members
+basicTexture::basicTexture(){
+    //Initialise variables
+    aTexture = NULL;
+    width = 0;
+    height = 0;
+}
+
+//destructor calls the free method
+basicTexture::~basicTexture(){
+    free();
+}
+
+//destroy texture and set attributes to 0
+void basicTexture::free(){
+    if(aTexture!=NULL){
+        SDL_DestroyTexture(aTexture);
+        aTexture = NULL;
+    }
+    width = 0;
+    height = 0;
+}
+
+//sets the blending mode for the texture
+void basicTexture::setBlendMode(SDL_BlendMode blending){
+    SDL_SetTextureBlendMode(aTexture,blending);
+}
+
+//sets the alpha blending
+void basicTexture::setAlpha(Uint8 alpha){
+    SDL_SetTextureAlphaMod(aTexture,alpha);
+}
+
+//actual rendering on the screen
+void basicTexture::render(SDL_Renderer* gameRenderer, int x, int y, double angle, SDL_Point* center){
+
+    //The rectangle on the screen to render
+    SDL_Rect renderRect = {x,y,width,height};
+
+    //Add to renderer
+    SDL_RenderCopyEx(gameRenderer,aTexture,NULL,&renderRect,angle,center,SDL_FLIP_NONE);
+}
+
+//return width
+int basicTexture::getWidth(){
+    return width;
+}
+
+//return height
+int basicTexture::getHeight(){
+    return height;
+}
+
+bool imgTexture::loadImgTexture(std::string path, SDL_Renderer* gameRenderer){
+    bool success = true;
+    
+    //Load image as surface
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if(loadedSurface == NULL){
+        std::cout<<"Failed to load image. SDL Error:"<<IMG_GetError();
+        success = false;
+        return success;
+    }
+
+    //Colour keying set to white 
+    SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0xFF, 0xFF, 0xFF));
+
+    //Create texture from the surface
+    SDL_Texture* newTexture = SDL_CreateTextureFromSurface(gameRenderer, loadedSurface);
+    if(newTexture == NULL){
+        std::cout<<"Failed to convert surface to texture. SDL Error:"<<SDL_GetError();
+        success = false;
+        return success;
+    }
+    width = loadedSurface->w;
+    height = loadedSurface->h;
+    
+    //Free up loaded surface
+    SDL_FreeSurface(loadedSurface);
+    loadedSurface = NULL;
+    aTexture = newTexture;
+    
+    return aTexture!=NULL;
+}
+
+bool textTexture::loadTextTexture(std::string textureText, SDL_Renderer* gameRenderer, SDL_Color textColor,TTF_Font* gameFont){
+    bool success = true;
+    SDL_Surface* textSurface = TTF_RenderText_Solid(gameFont,textureText.c_str(),textColor);
+    if(textSurface == NULL){
+        std::cout<<"Unable to render text surface! SDL_ttf Error:"<<TTF_GetError()<<std::endl;
+        success = false;
+        return success;
+    }
+    //Create a texture from the surface
+    aTexture = SDL_CreateTextureFromSurface(gameRenderer,textSurface);
+    if(aTexture == NULL){
+        std::cout<<"Failed to create texture from text surface! SDL Error: "<<SDL_GetError()<<std::endl;
+        success = false;
+        return success;
+    }
+
+    //set dimensions
+    width = textSurface->w;
+    height = textSurface->h;
+
+    SDL_FreeSurface(textSurface);
+
+    return aTexture!=NULL;
 }
--- src/rendering/texture.hpp
+++ src/rendering/texture.hpp
@@ -4,8 +4,9 @@
 #include <string>
 #include "SDL2/SDL.h"
 #include "SDL2_image/SDL_image.h"
+#include "SDL2_ttf/SDL_ttf.h"
 
-class gameTexture{
+/* class gameTexture{
     private:
     //actual hardware texture
     SDL_Texture* aTexture;
@@ -29,19 +30,59 @@ public:
     //Render Texture at given point
     void render(SDL_Renderer* gameRenderer, int x, int y, double angle = 0.0, SDL_Point* center = NULL);
     
-    //Set colour modulation
-    //void setColour(Uint8 red, Uint8 green, Uint8 blue);
-    
-    //Set Blend Mode
-    //void setBlendMode(SDL_BlendMode blending);
-    
-    //Set alpha modulation
-    //void setAlpha(Uint8 alpha);
+
     
     //Get Image dimensions
     int getWidth();
     int getHeight();
     
+};
+ */
+//basic Texture class
+class basicTexture{
+    protected:
+        //actual texture
+        SDL_Texture* aTexture;
+
+        //attributes
+        int width;
+        int height;
+    public:
+        //constructor
+        basicTexture();
+
+        //destructor
+        ~basicTexture();
+
+        void free();
+
+        //Set colour modulation
+        //void setColour(Uint8 red, Uint8 green, Uint8 blue);
+    
+        //Set Blend Mode
+        void setBlendMode(SDL_BlendMode blending = SDL_BLENDMODE_BLEND);
+    
+        //Set alpha modulation
+        void setAlpha(Uint8 alpha);
+
+        //renders image texture
+        void render(SDL_Renderer* gameRenderer, int x, int y, double angle = 0.0, SDL_Point* center = NULL);
+
+        //Get dimensions
+        int getWidth();
+        int getHeight();
+};
+
+class imgTexture : public basicTexture{
+    public:
+        //loads image texture
+        bool loadImgTexture(std::string path, SDL_Renderer* gameRenderer);
+};
+
+class textTexture : public basicTexture{
+    public:
+        //creates texture from text
+        bool loadTextTexture(std::string textureText, SDL_Renderer* gameRenderer, SDL_Color textColor,TTF_Font* gameFont);
 };
 
 #endif
